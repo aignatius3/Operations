743dfd2d27ee2a1a208b309c7f6f4d9a6bf66846
Ashwin Ignatius <aignatius3@gatech.edu>
2017-06-13 16:00:08 -0500

    Second commit

--- src/main/java/com/operations/arithmetic/ArithmeticApplication.java
+++ src/main/java/com/operations/arithmetic/ArithmeticApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArithmeticApplication {
 
 	public static void main(String[] args) {
+		System.out.println("This is an arithmetic application.");
 		SpringApplication.run(ArithmeticApplication.class, args);
 	}
 }
